4177c8a6e344d32e12dd8a1f4300c61655585613
Martin Garton <garton@gmail.com>
2025-07-05 14:01:54 +0100

    Simplify errors

--- repro_test_fdw/src/lib.rs
+++ repro_test_fdw/src/lib.rs
@@ -1,20 +1,15 @@
 use pgrx::pg_sys::panic::ErrorReport;
 use pgrx::prelude::*;
-use pgrx::PgSqlErrorCode;
 use std::collections::HashMap;
-use std::fmt::Display;
 use supabase_wrappers::prelude::*;
 
 pg_module_magic!();
 
-//extension_sql_file!("../sql/bootstrap.sql", bootstrap);
-//extension_sql_file!("../sql/finalize.sql", finalize);
-
-#[wrappers_fdw(error_type = "Error")]
+#[wrappers_fdw(error_type = "ErrorReport")]
 pub(crate) struct ReproTestFdw {}
 
-impl ForeignDataWrapper<Error> for ReproTestFdw {
-    fn new(_server: ForeignServer) -> Result<Self, Error> {
+impl ForeignDataWrapper<ErrorReport> for ReproTestFdw {
+    fn new(_server: ForeignServer) -> Result<Self, ErrorReport> {
         Ok(Self {})
     }
 
@@ -25,56 +20,31 @@ impl ForeignDataWrapper<Error> for ReproTestFdw {
         _sorts: &[Sort],
         _limit: &Option<Limit>,
         _options: &HashMap<String, String>,
-    ) -> Result<(), Error> {
+    ) -> Result<(), ErrorReport> {
         Ok(())
     }
 
-    fn iter_scan(&mut self, _output_row: &mut Row) -> Result<Option<()>, Error> {
+    fn iter_scan(&mut self, _output_row: &mut Row) -> Result<Option<()>, ErrorReport> {
         Ok(None)
     }
 
-    fn end_scan(&mut self) -> Result<(), Error> {
+    fn end_scan(&mut self) -> Result<(), ErrorReport> {
         Ok(())
     }
 
-    fn re_scan(&mut self) -> Result<(), Error> {
+    fn re_scan(&mut self) -> Result<(), ErrorReport> {
         Ok(())
     }
 
-    fn begin_modify(&mut self, _options: &HashMap<String, String>) -> Result<(), Error> {
+    fn begin_modify(&mut self, _options: &HashMap<String, String>) -> Result<(), ErrorReport> {
         Ok(())
     }
 
-    fn insert(&mut self, _row: &Row) -> Result<(), Error> {
+    fn insert(&mut self, _row: &Row) -> Result<(), ErrorReport> {
         Ok(())
     }
 
-    fn end_modify(&mut self) -> Result<(), Error> {
+    fn end_modify(&mut self) -> Result<(), ErrorReport> {
         Ok(())
-    }
-}
-
-#[derive(Debug)]
-pub enum Error {
-    Config(String),
-    Internal(String),
-    Unknown(String),
-    Query(String),
-}
-
-impl Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Error::Config(e) => write!(f, "config error : {e}"),
-            Error::Internal(e) => write!(f, "internal error : {e}"),
-            Error::Unknown(e) => write!(f, "unknown error : {e}"),
-            Error::Query(e) => write!(f, "query error : {e}"),
-        }
-    }
-}
-
-impl From<Error> for ErrorReport {
-    fn from(err: Error) -> Self {
-        ErrorReport::new(PgSqlErrorCode::ERRCODE_FDW_ERROR, format!("{err}"), "")
     }
 }
